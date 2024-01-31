28647d920d65e4ce53656aad947282848066cf76
PomiHD <wsgddjy@live.com>
2024-02-01 04:05:46 +1000

    102. Outputting Results in a List & Deriving More Values

--- src/Components/ResultTable.tsx
+++ src/Components/ResultTable.tsx
@@ -1,12 +1,43 @@
 ﻿import React from "react";
-import { formatter } from "../util/investment";
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
+
 export function ResultTable({ userInput }) {
-  console.log(userInput);
   const results = calculateInvestmentResults(userInput);
+  const initialInvestment =
+    results[0].valueEndOfYear -
+    results[0].interest -
+    results[0].annualInvestment;
   console.log(results);
   return (
-    <p>result...</p>
+    <table id="result">
+      <thead>
+        <tr>
+          <td>Year</td>
+          <td>Investment Value</td>
+          <td>Interest (Year)</td>
+          <td>Total Interest</td>
+          <td>Invest Capital</td>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((result) => {
+          const totalInterest =
+            result.valueEndOfYear -
+            initialInvestment -
+            result.annualInvestment * result.year;
+          const totalCapitalInvested = result.valueEndOfYear - totalInterest;
+          return (
+            <tr key={result.year}>
+              <td>{result.year} </td>
+              <td>{formatter.format(result.valueEndOfYear)}</td>
+              <td>{formatter.format(result.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalCapitalInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
     // <>
     //   <table id={"result"}>
     //     <thead id={"result"}>
